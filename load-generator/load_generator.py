ab823a0afeca9f0fdd9e3c4cfc4396ce257d7f88
louisloechel <57091589+louisloechel@users.noreply.github.com>
2024-09-16 17:20:22 +0200

    keep load genrator alive forever

--- load-generator/load_generator.py
+++ load-generator/load_generator.py
@@ -5,18 +5,25 @@ import time
 import os
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'kafka:9092')
+CSV_FILE = 'data.csv'
+TOPIC = 'topicA'
+SLEEP_INTERVAL = 1  # Time in seconds between messages
 
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
-with open('data.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        producer.send('topicA', value=row)
-        print(f"Sent: {row}")
-        time.sleep(1)
-
-producer.flush()
-producer.close()
+try:
+    while True:
+        with open(CSV_FILE, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                producer.send(TOPIC, value=row)
+                print(f"Sent: {row}")
+                time.sleep(SLEEP_INTERVAL)
+except KeyboardInterrupt:
+    print("Stopping load generator...")
+finally:
+    producer.flush()
+    producer.close()
