88082a57441202cb895e2a1228c70cb0e8c24a50
louisloechel <57091589+louisloechel@users.noreply.github.com>
2025-07-20 21:14:49 +0200

    set k=5

--- og-prink/src/main/java/ganges/GangesEvaluation.java
+++ og-prink/src/main/java/ganges/GangesEvaluation.java
@@ -43,7 +43,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class GangesEvaluation {
 
-    int k = 11;
+    int k = 5;
     int l = 3;
     int delta = 20;
     int beta = 50;
